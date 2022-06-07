8b7fa15a03ce00dabca382f144aa2d7dfe46fd5e
Anton Cagle <anton.cagle@landsend.com>
2022-06-07 12:21:17 -0500

    created sample file

--- amazon-reviews/src/main/scala/com/centaurcomputing/spark/amazonReviews.scala
+++ amazon-reviews/src/main/scala/com/centaurcomputing/spark/amazonReviews.scala
@@ -1,6 +1,7 @@
 package com.centaurcomputing
 import org.apache.log4j._
-import org.apache.spark.sql.{Encoders, SparkSession}
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.{DataFrame, Encoders, SparkSession}
 
 object amazonReviews {
 
@@ -25,6 +26,8 @@ object amazonReviews {
     val reviews = spark.read
       .json(path)
       //.printSchema()
+
+    //schema for original dataset
 //    |-- asin: string (nullable = true)
 //    |-- helpful: array (nullable = true)
 //    |    |-- element: long (containsNull = true)
@@ -35,15 +38,53 @@ object amazonReviews {
 //    |-- reviewerName: string (nullable = true)
 //    |-- summary: string (nullable = true)
 //    |-- unixReviewTime: long (nullable = true)
+
+    //a couple of queries to test shape and contents of the data
     spark.sqlContext.sql("CREATE TEMPORARY VIEW musicReviews USING json OPTIONS" +
       " (path 'data/reviews_CDs_and_Vinyl_5.json')")
-      val distinctReviewers = spark.sql("SELECT reviewerID,reviewCount" +
-        " FROM (SELECT reviewerID, COUNT(reviewText) reviewCount from musicReviews GROUP BY reviewerID) WHERE reviewCount > 100 ")
-          distinctReviewers.show()
+//      val distinctReviewers = spark.sql("SELECT reviewerID,reviewCount" +
+//        " FROM (SELECT reviewerID, COUNT(reviewText) reviewCount from musicReviews GROUP BY reviewerID) WHERE reviewCount > 100 ")
+//          distinctReviewers.show()
+//
+//      val topReveiwers = distinctReviewers.filter(distinctReviewers("reviewCount") > 1000).count()
+//      println("Number of Top Reviewers: " + topReveiwers)
+
+  //make the dataset smaller to run some testing
+  val reviewSample = spark.sql("SELECT reviewerID,asin, STRING(overall),reviewText" +
+          " FROM musicReviews").sample(.10)
+    reviewSample
+      .write
+      .options(Map("header"->"true", "delimiter"->","))
+      .csv("data/reviewSample.csv")
+  //data cleaning
+
+    //text preprocessing pipeline
+    // 1. remove html tags
+    // 2. remove accented characters
+    // 3. expand contractions
+    // 4. remove punctuation
+    // 5. lemmatization
+
+
+    def upper(x: String) : String = {
+      x.toUpperCase()
+    }
+    //.toUpperCase lambda function
+    def lower(x: String) : String = {
+      x.toLowerCase()
+    }
+    def removePunctuation(x: String) : String = {
+      x.replaceAll("""[\p{Punct}]""", "")
+    }
+
+    //executing a function on list items
+//    val myList = List("Here is an Unformatted Sentence@!", "Here is AnOThEr UnFoRmAtTeD String.", "?Que, one More?")
+//    println(myList.)
+//
+//    val cleanedList = myList.map( (sentence: String) => (removePunctuation(sentence).toLowerCase()) )
 
 
 
-    val topReveiwers = distinctReviewers.filter(distinctReviewers("reviewCount") > 1000).count()
-    println("Number of Top Reviewers: " + topReveiwers)
+
     spark.stop()
   }}
